1874b577b995160cea2a8cb07a62a0b2a1692943
Shawn Shah <41961267+sshah-wework@users.noreply.github.com>
2019-06-18 08:58:32 -0700

    set document title and favicon (#17)
    * set document title
    
    * set favicon to wework favicon

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import MainContainer from './components/MainContainer'
 import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
 import grey from '@material-ui/core/colors/grey';
+import { Helmet } from 'react-helmet'
 
 const theme = createMuiTheme({
   palette: {
@@ -15,10 +16,15 @@ const theme = createMuiTheme({
   },
 });
 
+const TITLE = 'Marquez | Data Kit'
+
 class App extends Component {
   render() {
     return (
       <MuiThemeProvider theme={theme}>
+        <Helmet>
+          <title>{ TITLE }</title>
+        </Helmet>
         <MainContainer/>
       </MuiThemeProvider>
     );
