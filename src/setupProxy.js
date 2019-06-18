0df6e0a6c4f03c6bb6f3de0005dbf9f3bda98d6f
Shawn Shah <41961267+sshah-wework@users.noreply.github.com>
2019-06-18 16:28:57 -0700

    use *_PORT env vars set by wek8s (#18)

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,16 +1,18 @@
 const proxy = require('http-proxy-middleware');
 
 module.exports = function(app) { 
+  // using MARQUEZ_SERVICE_SERVICE_PORT which is set by wek8s
   var apiOptions = {
-    target: `http://${process.env.MARQUEZ_SERVICE_HOST}:${process.env.MARQUEZ_SERVICE_PORT}/`,
+    target: `http://${process.env.MARQUEZ_SERVICE_HOST}:${process.env.MARQUEZ_SERVICE_SERVICE_PORT}/`,
   };
 
   var govRewriteFn = function(path, req) {
     return path.replace('/governance/api', '/api');
   }; 
 
+  // using MARQUEZ_GOVERNANCE_SERVICE_SERVICE_PORT_HTTP here which is set by wek8s
   var govOptions = {
-    target: `http://${process.env.MARQUEZ_GOVERNANCE_HOST}:${process.env.MARQUEZ_GOVERNANCE_PORT}/`,
+    target: `http://${process.env.MARQUEZ_GOVERNANCE_HOST}:${process.env.MARQUEZ_GOVERNANCE_SERVICE_SERVICE_PORT}/`,
     pathRewrite: govRewriteFn
   };
 
