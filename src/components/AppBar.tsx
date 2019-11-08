25f7b013439a4e0f14b020a6d94f87d02cfca656
Grant Foster <grantdfoster@gmail.com>
2019-11-08 15:35:53 -0500

    Feature/navbar updates (#86)
    * revises navbar

--- src/components/AppBar.tsx
+++ src/components/AppBar.tsx
@@ -1,5 +1,4 @@
 import React, { ReactElement } from 'react'
-import { Link } from 'react-router-dom'
 import { AppBar, Toolbar, Typography } from '@material-ui/core'
 import {
   withStyles,
@@ -7,30 +6,22 @@ import {
   WithStyles as IWithStyles,
   Theme as ITheme
 } from '@material-ui/core/styles'
-import Box from '@material-ui/core/Box'
-import 'typeface-rajdhani'
-import '../img/we-logo.png'
 
-const styles = ({ spacing, zIndex }: ITheme) => {
+import Menu from './Menu'
+
+const styles = ({ zIndex }: ITheme) => {
   return createStyles({
     appBar: {
-      zIndex: zIndex.drawer + 1
+      zIndex: zIndex.drawer + 1,
+      height: '0px'
     },
     rightToolbar: {
       marginLeft: 'auto',
       marginRight: 2.5
     },
-    feedbackButton: {
-      padding: spacing(1),
-      backgroundColor: '#2B2B33',
-      color: 'white',
-      fontSize: 15,
-      borderWidth: 1.2,
-      '&:hover': {
-        color: '#71ddbf',
-        cursor: 'pointer',
-        borderColor: '#71ddbf'
-      }
+    icon: {
+      fontFamily: 'Karla',
+      fontWeight: 'bold'
     }
   })
 }
@@ -42,23 +33,11 @@ const MyAppBar = (props: IProps): ReactElement => {
   return (
     <AppBar position='fixed' className={classes.appBar}>
       <Toolbar>
-        <Box mr={2}>
-          <img src='img/we-logo.png' height={50} alt='WeWork Logo' />
-        </Box>
-        <Typography
-          variant='h4'
-          color='inherit'
-          noWrap
-          style={{ fontFamily: 'rajdhani', fontWeight: 'bold' }}
-        >
-          <Link to='/' className='link'>
-            MARQUEZ
-          </Link>
+        <Typography className={classes.icon} variant='h4' color='inherit' noWrap>
+          MARQUEZ
         </Typography>
         <div className={classes.rightToolbar}>
-          <a href='https://forms.gle/gWj8fn8iYiTsKEPD6' target='_blank' rel='noopener noreferrer'>
-            <button className={classes.feedbackButton}> Feedback </button>
-          </a>
+          <Menu></Menu>
         </div>
       </Toolbar>
     </AppBar>
