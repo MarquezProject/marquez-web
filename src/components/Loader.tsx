f1d3086c52db5cf5a706233f8776b40ca8e17e6e
mell0kat <katherine.mello@wework.com>
2019-11-20 11:33:00 -0500

    Adds loading component

---
+++ src/components/Loader.tsx
@@ -0,0 +1,8 @@
+import * as React from 'react'
+import CircularProgress from '@material-ui/core/CircularProgress'
+
+interface IProps {}
+
+export default (props: IProps) => {
+  return <CircularProgress color='secondary' />
+}
