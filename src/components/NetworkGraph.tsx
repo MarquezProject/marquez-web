fc42d65d478f6a8bf3f87c3339ad479c60ed0d8d
Grant Foster <grantdfoster@gmail.com>
2020-02-27 13:44:15 -0500

    implements helper functions leading up to graph / svg implementation
    Signed-off-by: Grant Foster <grantdfoster@gmail.com>

--- src/components/NetworkGraph.tsx
+++ src/components/NetworkGraph.tsx
@@ -10,21 +10,25 @@ import {
 
 import Legend from './Legend'
 
-// import { createNetworkData } from '../helpers'
 import { IDataset, IJob } from '../types/'
 
+import _find from 'lodash/find'
+import _filter from 'lodash/filter'
+import _flatten from 'lodash/flatten'
+import _map from 'lodash/map'
+import _sortBy from 'lodash/sortBy'
+
 import { select } from 'd3-selection'
+import { hierarchy } from 'd3-hierarchy'
+
 // import { tree } from 'd3-hierarchy'
 // import { linkHorizontal } from 'd3-shape'
 
-// import { forceCenter, forceLink, forceSimulation, forceManyBody, forceY } from 'd3-force'
 import { zoom } from 'd3-zoom'
-// import { color } from 'd3-color'
 import Loader from './Loader'
+// import { IDatasetAPI } from '../types/api'
 // const globalStyles = require('../global_styles.css')
 // const { jobNodeGrey, linkGrey, datasetNodeWhite } = globalStyles
-
-// const fadedOut = (color(jobNodeGrey) as any).darker(1.7).toString()
 
 const styles = ({ palette }: Theme) => {
   return createStyles({
@@ -69,14 +73,12 @@ type IAllProps = IWithStyles<typeof styles> & IProps
 export class NetworkGraph extends React.Component<IAllProps, {}> {
   shouldComponentUpdate(newProps: IProps) {
     
-    // const networkData = createNetworkData(newProps.datasets, newProps.jobs)
-    // const { nodes, links } = networkData
-
     const svg: d3.Selection<SVGElement, void, HTMLElement, void> = select('#network-graph')
 
     if (svg.empty()) {
       return true
     }
+
     // const width = +svg.style('width').replace('px', '')
     // const height = +svg.style('height').replace('px', '')
 
@@ -88,30 +90,82 @@ export class NetworkGraph extends React.Component<IAllProps, {}> {
         })
     )
 
-    // const linkContainer: d3.Selection<SVGElement, void, HTMLElement, void> = svg.select('#links')
+    const isDataset = (node: any) => {
+      const name = node.name || node.data.name
+      return _find(newProps.datasets, d => d.name == name)
+    }
 
+    const findChildren = (node: any) => {
+      let children
+      if (isDataset(node)) {
+        children = _filter(newProps.jobs, j => j.inputs.includes(node.name))
+      } else {
+        const job = _find(newProps.jobs, j => j.name == node.name)
+        children = job ? _filter(newProps.datasets, d => job.outputs.includes(d.name) && !job.inputs.includes(d.name)) : []
+      }
+      return children
+    }
+
+    const findParents = (node: any) => {
+      let parents
+      if (isDataset(node)) {
+        parents = _filter(newProps.jobs, j => j.outputs.includes(node.name))
+      } else {
+        const job = _find(newProps.jobs, j => j.name == node.name)
+        parents = job ? _filter(newProps.datasets, j => job.inputs.includes(j.name) && !job.outputs.includes(j.name)) : []
+      }
+      return parents
+    }
+
+    const getLineages = () => {
+      const searchedDatasets = _filter(newProps.datasets, d => d.matches)
+      const searchedJobs = _filter(newProps.jobs, j => j.matches)
+      const allNodes = [...searchedDatasets, ...searchedJobs]
+      
+      const lineages = _map(allNodes, (rootNode: any) => {
+        rootNode.children = findChildren(rootNode)
+        let children = rootNode.children
+        while (children.length > 0) {
+          _map(children, child => {
+            child.children = findChildren(child)
+          })
+          children = _flatten(_map(children, chi => chi.children))
+        }
+        return rootNode
+      })
+      return lineages
+    }
+
+    const getReverseLineage = (node: any) => {
+      node.children = findParents(node)
+      let children = node.children
+      while (children.length > 0) {
+        _map(children, child => {
+          child.children = findParents(child)
+        })
+        children = _flatten(_map(children, chi => chi.children))
+      }
+      return node
+    }
+
+    // run calculations for network graph
+    const lineages = getLineages()
+    let clusters = _map(lineages, lineage => hierarchy(lineage))
+    clusters = _sortBy(clusters, l => l.descendants().length)
+    const largestCluster = clusters[clusters.length - 1]
+    if (largestCluster) {
+      const rootNode = largestCluster.data
+      const reverseLineage = getReverseLineage(rootNode)
+      const reverseCluster = hierarchy(reverseLineage)
     
-
-    // const tooltip = select('#tooltip')
-
-    // datasetNodeSelection
-    //   .on('mouseover', focus)
-    //   .on('mousemove', move)
-    //   .on('mouseout', unfocus)
-
-    // function focus(d: INodeNetwork) {
-    //   tooltip.text(d.id).style('visibility', 'visible')
-    // }
-
-    // function move() {
-    //   return tooltip
-    //     .style('top', (event as any).pageY - 10 + 'px')
-    //     .style('left', (event as any).pageX + 10 + 'px')
-    // }
-
-    // function unfocus() {
-    //   return tooltip.style('visibility', 'hidden')
-    // }
+      // remove svg elements
+      svg.selectAll('*').remove()
+    
+      // graph(largestCluster, false)
+      // graph(reverseCluster, true)
+      console.log(largestCluster)
+      console.log(reverseCluster)
+    }
 
     if (this.props.isLoading !== newProps.isLoading) {
       return true
