29c0c6537722300fb87d9702abd54d7bc4bf95d6
Grant Foster <grantdfoster@gmail.com>
2020-04-01 10:17:08 -0400

    updates routing

--- setupProxy.js
+++ setupProxy.js
@@ -1,16 +1,16 @@
 const proxy = require('http-proxy-middleware')
 const express = require('express')
 
-var apiOptions = {
+const apiOptions = {
   target: `http://${process.env.MARQUEZ_HOST}:${process.env.MARQUEZ_PORT}/`
 }
+const app = express()
+const path = __dirname + '/dist'
 
-var app = express()
-
-const path = __dirname + '/dist/'
-app.use(express.static(path))
+app.use('/', express.static(path))
+app.use('/datasets/:name', express.static(path))
+app.use('/jobs/:name', express.static(path))
 app.use(proxy('/api/v1', apiOptions))
-app.use(proxy('/', apiOptions))
 
 app.listen(3000, function() {
   console.log('App listening on port 3000!')
